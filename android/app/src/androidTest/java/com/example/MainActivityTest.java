25c890f3e7fb8ea1235d97087c3caf6f657b5453
adibaalya <216106@student.upm.edu.my>
2024-05-19 09:07:19 +0800

    my changes

--- android/app/src/androidTest/java/com/example/MainActivityTest.java
+++ android/app/src/androidTest/java/com/example/MainActivityTest.java
@@ -1,3 +1,10 @@
+package com.example.factoryApps;
+
+import androidx.test.rule.ActivityTestRule;
+import dev.flutter.plugins.integration_test.FlutterTestRunner;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
 public class MainActivityTest {
     
 }
