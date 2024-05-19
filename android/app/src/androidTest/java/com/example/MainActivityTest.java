867d38bd328c191179f83bc4bddfe39a30b0f94a
adibaalya <216106@student.upm.edu.my>
2024-05-19 15:14:58 +0800

    update

--- android/app/src/androidTest/java/com/example/MainActivityTest.java
+++ android/app/src/androidTest/java/com/example/MainActivityTest.java
@@ -1,4 +1,4 @@
-package com.example.mobile_app_lab_5;
+package com.example.factory_apps;
 
 import androidx.test.rule.ActivityTestRule;
 import dev.flutter.plugins.integration_test.FlutterTestRunner;
